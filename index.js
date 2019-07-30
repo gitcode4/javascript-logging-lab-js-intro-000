944260c9afa4fb016aa8e143beeb53fb6d9f8fe1
Liz <lizphil88@gmail.com>
2019-07-30 02:11:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.error("HALP!")
 console.log("I would be a logger.")
 console.warn("Fix Error!")
+var word;
+word = 'bird';
+console.log(word);
+word = 'dog';
+var word = 'bird';
+window.word;
+var a = 5, b = 2, c = 3, d = 'hello', e = 'goodbye'
